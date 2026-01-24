2c0fa2c15eeb3403eeae925f4412a0a1470143ad
Miguel <miguelkunzer567@gmail.com>
2026-01-24 13:36:56 -0300

    Fixado caminhos errados. - v1.1.2

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 
-import { createRoot } from 'react-dom/client'
-import './styles/index.css'
-import App from './App.jsx';
+import { createRoot } from 'react-dom/client';
+import '@/styles/index.css';
+import App from '@/App.jsx';
 
 createRoot(document.body).render(
   <App></App>
